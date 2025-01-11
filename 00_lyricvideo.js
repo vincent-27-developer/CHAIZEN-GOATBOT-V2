8b1b69445b43039ac3fdbbc42469e10d6e81249c
Vincent Armenion <vincentarmenion71@gmail.com>
2025-01-11 14:57:23 +0800

    Update and rename lyricvideo.js to 00_lyricvideo.js

--- 00_lyricvideo.js
+++ 00_lyricvideo.js
@@ -14,7 +14,7 @@ async function fetchTikTokVideos(query) {
 module.exports = {
   config: {
     name: "lyricvideo",
-    aliases: ['lv'],
+    aliases: ["lv"],
     author: "Vincent Armenion",
     version: "1.0",
     shortDescription: {
