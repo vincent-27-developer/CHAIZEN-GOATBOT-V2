c716cbaabdc4053cc73969c77f2222b3ee3a6885
Vincent Armenion <vincentarmenion71@gmail.com>
2025-01-12 14:24:15 +0800

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -2,59 +2,58 @@ const fs = require('fs');
 const moment = require('moment-timezone');
 
 module.exports = {
-	config: {
-		name: "info",
-		version: "1.0",
-		author: "armenion",
-		countDown: 20,
-		role: 0,
-		shortDescription: { vi: "", en: "" },
-		longDescription: { vi: "", en: "" },
-		category: "owner",
-		guide: { en: "" },
-		envConfig: {}
-	},
-	onStart: async function ({ message }) {
-		const botName = "Chaizen Zee";
-		const botPrefix = "=";
-		const authorName = "Vincent Armenion";
-		const ownAge = "17";
-		const teamName = "Philippines Team";
-		const authorFB = "https://www.facebook.com/100090775159086";
-		const authorInsta = "Thead469";
-		const tikTok = "tiktok.com/@Cliffthegreat0";
-		const urls = JSON.parse(fs.readFileSync('cliff.json'));
-		const link = urls[Math.floor(Math.random() * urls.length)];
-		const now = moment().tz('Asia/Jakarta');
-		const date = now.format('MMMM Do YYYY');
-		const time = now.format('h:mm:ss A');
-		const uptime = process.uptime();
-		const seconds = Math.floor(uptime % 60);
-		const minutes = Math.floor((uptime / 60) % 60);
-		const hours = Math.floor((uptime / (60 * 60)) % 24);
-		const days = Math.floor(uptime / (60 * 60 * 24));
-		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+  config: {
+    name: "info",
+    version: "1.3",
+    author: "Vincent Armenion",
+    countDown: 5,
+    role: 0,
+    shortDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin along with an image."
+    },
+    longDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin along with an image."
+    },
+    category: "utility",
+    guide: {
+      en: "{pn}"
+    },
+    envConfig: {}
+  },
 
-		message.reply({
-			body: `《  Bot & Owner Info 》
-\Name: ${botName}
-\Bot Prefix: ${botPrefix}
-\owner: ${authorName}
-\age : ${ownAge}
-\Facebook: ${authorFB}
-\Instagram: ${authorInsta}
-\TikTok: ${tikTok}
-\Datee: ${date}
-\Time: ${time}
-\Team: ${teamName}
-\Uptime: ${uptimeString}
-\===============`,
-			attachment: await global.utils.getStreamFromURL(link)
-		});
-	},
-	onChat: async function ({ event, message, getLang }) {
-		if (event.body && event.body.toLowerCase() === "info") {
-			this.onStart({ message });
-		}
-	}
+  onStart: async function ({ message }) {
+    const botName = "『=』➡︎ 『 Ｏｗｎｅｒ 』 𝕮𝖍𝖆𝖎𝖟𝖊𝖓 𝕲𝖔𝖆𝖙𝖇𝖔𝖙𝗩𝟮";
+    const botPrefix = "=";
+    const authorName = "Vincent Armenion";
+    const authorFB = "https://www.facebook.com/100090775159086";
+    const authorInsta = "";
+    const status = "hi, crush ko po kayong lahat";
+
+    const urls = JSON.parse(fs.readFileSync('loid.json'));
+    const link = urls[Math.floor(Math.random() * urls.length)];
+
+    const now = moment().tz('Asia/Manila');
+    const date = now.format('MMMM Do YYYY');
+    const time = now.format('h:mm:ss A');
+
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+    message.reply({
+      body: `===「 Bot & Owner Info 」===\n❏Bot Name: ${botName}\n❏Bot Prefix: ${botPrefix}\n❏Name: ${authorName}\n❏Facebook: ${authorFB}\n❏Instagram: ${authorInsta}\n❏Status: ${status}\n❏Date: ${date}\n❏Time: ${time}\n❏Uptime: ${uptimeString}\n=====================`,
+      attachment: await global.utils.getStreamFromURL(link)
+    });
+  },
+
+  onChat: async function({ event, message, getLang }) {
+    if (event.body && event.body.toLowerCase() === "info") {
+      this.onStart({ message });
+    }
+  }
 };
